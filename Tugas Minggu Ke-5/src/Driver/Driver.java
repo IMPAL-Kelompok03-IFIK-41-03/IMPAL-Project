7fec5246e5528705b371ff1ffe1e3dfdfbd9ca49
Irsyad Muhamad <54497828+irsyadmuhamad@users.noreply.github.com>
2019-09-22 16:55:28 +0700

    Upload Source Code NgampusYuk
    Upload Source Code NgampusYuk

---
+++ Tugas Minggu Ke-5/src/Driver/Driver.java
@@ -0,0 +1,12 @@
+package Driver;
+
+import Controller.*;
+import Model.*;
+import java.sql.SQLException;
+
+public class Driver {
+    public static void main(String[] args) throws Exception {
+        new controllerLogin();
+    }
+    
+}
